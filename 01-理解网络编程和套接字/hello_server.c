1debe162e061adc26c5b5249d8ceb7e0bf2b0859
RuiyuChen <Abyssfish@users.noreply.github.com>
2021-03-01 20:23:51 +0800

    Update hello_server.c
    加上注释便于阅读理解

--- 01-理解网络编程和套接字/hello_server.c
+++ 01-理解网络编程和套接字/hello_server.c
@@ -13,7 +13,7 @@ int main(int argc, char* argv[]){
         exit(1);
     }
 
-    int sockServ = socket(PF_INET, SOCK_STREAM, 0);
+    int sockServ = socket(PF_INET, SOCK_STREAM, 0);//调用socket 函数(安装电话机)时进行的对话
 
     struct sockaddr_in sockServAddr;
     memset(&sockServAddr, 0, sizeof(sockServAddr));
@@ -21,18 +21,18 @@ int main(int argc, char* argv[]){
     sockServAddr.sin_addr.s_addr = htonl(INADDR_ANY);
     sockServAddr.sin_port = htons(atoi(argv[1]));
 
-    if(bind(sockServ, (struct sockaddr*)& sockServAddr, sizeof(sockServAddr)) ==-1){
+    if(bind(sockServ, (struct sockaddr*)& sockServAddr, sizeof(sockServAddr)) ==-1){//调用bind函数(分配电话号码)时进行的对话
         errorHandling("bind() error!");
     }
 
-    if(listen(sockServ, 5) == -1){
+    if(listen(sockServ, 5) == -1){//调用listen函数(连接电话线)时进行的对话
         errorHandling("listen() error!");
     }
 
     struct sockaddr_in sockClientAddr;
     socklen_t clientAddrLen = sizeof(sockClientAddr);
 
-    int sockClient = accept(sockServ, (struct sockaddr*)& sockClientAddr, &clientAddrLen);
+    int sockClient = accept(sockServ, (struct sockaddr*)& sockClientAddr, &clientAddrLen);//调用accept函数(拿起话筒)时进行的对话
     if(sockClient == -1){
         errorHandling("accept() error!");
     }
